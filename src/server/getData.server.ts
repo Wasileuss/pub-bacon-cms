511688e9f179d110dd96d74e0a10c160d9e5e4a2
Wasileuss <121878161+Wasileuss@users.noreply.github.com>
2025-02-20 15:07:02 +0200

    refactor: changed menu page

--- src/server/getData.server.ts
+++ src/server/getData.server.ts
@@ -4,5 +4,7 @@ import { collection, getDocs, query } from "firebase/firestore";
 export async function getItems(category: string): Promise<Item[]> {
     const q = query(collection(db, category));
     const querySnapshot = await getDocs(q);
+    const items = querySnapshot.docs.map(doc => doc.data());
+    console.log("Дані з Firebase:", items);
     return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as Item[];
 }
